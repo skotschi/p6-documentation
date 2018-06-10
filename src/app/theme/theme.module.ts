7934a1224c9e840aa57eb002f15075b9335f25bb
unknown <steffen.kotschi@outlook.com>
2018-06-10 22:57:41 +0200

    Added basic Navigation structure

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -5,22 +5,24 @@ import { PostComponent } from './components/post/post.component';
 import { RouterModule } from '@angular/router';
 import { Http, HttpModule } from '@angular/http';
 import { PagesService } from './services/pages.service';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 
 // Entry Points
 const DIRECTIVES = [
-]
+];
 
 // Components
 const COMPONENTS = [
   PostComponent,
-]
+  NavigationComponent
+];
 
 // modules
 const MODULES = [
   RouterModule,
   HttpModule
-]
+];
 
 @NgModule({
   imports: [
@@ -30,11 +32,12 @@ const MODULES = [
   declarations: [
     ...DIRECTIVES,
     ...COMPONENTS,
+    NavigationComponent,
   ],
   entryComponents: [
     ...DIRECTIVES
   ],
-  providers:[
+  providers: [
     AsyncPipe,
     PagesService
   ],
