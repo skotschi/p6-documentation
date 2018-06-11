33c487cb7463e914b95b96f015f9dd9d7351d3a5
Steffen <steffen.kotschi@outlook.com>
2018-06-11 20:55:03 +0200

    Added basic structure

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DesignComponent } from './pages/design/design.component';
+import { ConceptComponent } from './pages/concept/concept.component';
+import { FormalitiesComponent } from './pages/formalities/formalities.component';
+import { ManagementComponent } from './pages/management/management.component';
+import { TechComponent } from './pages/tech/tech.component';
 
 const App_ROUTES: Routes = [
     // {
@@ -22,6 +26,22 @@ const App_ROUTES: Routes = [
         path: 'design/:article',
         component: DesignComponent
     },
+    {
+        path: 'concept',
+        component: ConceptComponent
+    },
+    {
+        path: 'formalities',
+        component: FormalitiesComponent
+    },
+    {
+        path: 'management',
+        component: ManagementComponent
+    },
+    {
+        path: 'technology',
+        component: TechComponent
+    }
 ];
 
 
---
+++ src/app/pages/formalities/formalities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formalities',
+  templateUrl: './formalities.component.html',
+  styleUrls: ['./formalities.component.scss']
+})
+export class FormalitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
