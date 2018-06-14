9be9cc5c5674864322bee4814b36a1b4b6c22ede
Steffen <steffen.kotschi@outlook.com>
2018-06-14 14:08:06 +0200

    Minor bug fixes in page-service

--- src/app/theme/services/pages.service.ts
+++ src/app/theme/services/pages.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from 'selenium-webdriver/http';
 import { Http } from '@angular/http';
 import { Subject } from 'rxjs/Subject';
-import * as frontmatter from 'front-matter'
+import * as frontmatter from 'front-matter';
 
 
 @Injectable()
 export class PagesService {
 
-  constructor(private http:Http) { }
+  constructor(private http: Http) { }
 
-  loadMarkDownFromPath(pagePath:string):Promise<any> {
+  loadMarkDownFromPath(pagePath: string): Promise<any> {
     return this.http
       .get('assets/articles/' + pagePath + '.md')
       .toPromise()
@@ -21,14 +21,14 @@ export class PagesService {
       );
   }
 
-  loadPageFromPath(page:string) :Promise<{md:string, data:any}> {
+  loadPageFromPath(page: string): Promise<{md: string, data: any}> {
     return this.loadMarkDownFromPath(page).then((result) => {
-      let parse = frontmatter(result);
+      const parse = frontmatter(result);
       return {
         md: parse.body,
         data: parse.attributes
-      }
-    })
+      };
+    });
   }
 
 }
